0e46eac0314e3570a776a1e0f56712f399fda7d7
t7abhay <abhaytaras18@gmail.com>
2025-01-07 21:52:43 +0530

    change logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,17 +3,14 @@ import { IoIosVideocam } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 function Logo({ size = "30" }) {
-    return (
-        <>
-            <Link to={'/'} className="flex gap-2 items-center">
-                <IoIosVideocam
-                    size={size}
-                    color="#A855F7"
-                />
-                <span className="font-bold text-white">STREAMIFY</span>
-            </Link>
-        </>
-    );
+  return (
+    <>
+      <Link to={"/"} className="flex gap-2 items-center">
+        <IoIosVideocam size={size} color="#A855F7" />
+        <span className="font-bold text-white">V-STREAM</span>
+      </Link>
+    </>
+  );
 }
 
 export default Logo;
