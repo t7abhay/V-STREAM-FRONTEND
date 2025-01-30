5ab6fa519e0cd298898482e033e07daa871f5382
t7abhay <abhaytaras18@gmail.com>
2025-01-30 15:49:47 +0530

    d

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,9 +1,27 @@
 import axios from "axios";
 import { BASE_URL } from "../constants.js";
 
-const axiosInstance = axios.create();
+const getCsrfToken = () => {
+    const match = document.cookie.match(/XSRF-TOKEN=([^;]+)/);
+    return match ? decodeURIComponent(match[1]) : null;
+};
 
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
+
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true,
+});
+
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const csrfToken = getCsrfToken();
+        if (csrfToken) {
+            config.headers["X-XSRF-TOKEN"] = csrfToken;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
